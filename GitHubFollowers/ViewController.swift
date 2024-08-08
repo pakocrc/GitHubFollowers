746eed4a9716cfbecfcef00615be3c46b54062ae
Francisco Cordoba <pako.cordoba10@gmail.com>
2024-08-08 16:51:00 -0600

    Base code

--- GitHubFollowers/ViewController.swift
+++ GitHubFollowers/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        view.backgroundColor = .orange
     }
-
-
 }
 
+#Preview {
+    ViewController()
+}
